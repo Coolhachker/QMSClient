b667070125b34892d73bcb41194ebda7c8c2d701
unknown <egorbarabas77@gmail.com>
2025-08-28 11:22:55 +0300

    забыл

--- src/databases/sqlite_db/scripts/set_device_name.py
+++ src/databases/sqlite_db/scripts/set_device_name.py
@@ -3,4 +3,5 @@ from src.databases.sqlite_db.engine_sqlite_db import sqlite_engine
 
 while True:
 	device_name: str = input("Введите имя устройства > ")
-	
+	sqlite_engine.update_device_name(device_name)
+	break
